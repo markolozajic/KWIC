5516ea0bf0c752c1984611043706ea2949f4017d
markolozajic <marko.lozajic@student.uni-tuebingen.de>
2017-07-09 21:06:46 +0200

    POSTagging should now work for sample file

--- src/POSTagging.java
+++ src/POSTagging.java
@@ -3,30 +3,22 @@ import java.io.*;
 
 public class POSTagging {
     public static void main (String[] args) throws IOException{
+
             File testfile = new File("testfile.txt");
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            Process p = runtime.exec("~/IdeaProjects/KWIC/opennlp_master/bin/opennlp");
 
-            InputStream inStream = p.getInputStream();
-            InputStreamReader sReader = new InputStreamReader(inStream);
-            BufferedReader buff = new BufferedReader(sReader);
-            String nextLine = "";
-            while((nextLine = buff.readLine()) != null) {
-                System.out.println(nextLine);
-            }
-        } catch (IOException e) {
-            System.out.println("Whoops! Something went wrong");
-        }
+            ProcessBuilder sentences = new ProcessBuilder("opennlp-master/bin/opennlp", "SentenceDetector", "opennlp-master/models/en-sent.bin");
+            sentences.redirectInput(testfile);
+            sentences.redirectOutput(new File("sentences.txt"));
+            sentences.start();
 
-//            ProcessBuilder test = new ProcessBuilder("ls");
-//            test.redirectOutput(new File("list.txt"));
-//            test.start();
-//
-//            ProcessBuilder builder = new ProcessBuilder("opennlp_master/bin/opennlp","TokenizerME","models/en-token.bin");
-//            builder.redirectInput(testfile);
-//            builder.redirectOutput(new File("tokenizedfile.txt"));
-//            builder.start();
+            ProcessBuilder tokenizer = new ProcessBuilder("opennlp-master/bin/opennlp","TokenizerME","opennlp-master/models/en-token.bin");
+            tokenizer.redirectInput(new File("sentences.txt"));
+            tokenizer.redirectOutput(new File("tokenized.txt"));
+            tokenizer.start();
+
+            ProcessBuilder posTagger = new ProcessBuilder("opennlp-master/bin/opennlp","POSTagger","opennlp-master/models/en-pos-maxent.bin");
+            posTagger.redirectInput(new File("tokenized.txt"));
+            posTagger.redirectOutput(new File("tagged.txt"));
+            posTagger.start();
     }
-
 }

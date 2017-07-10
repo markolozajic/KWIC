91f9ce31d9154fef7ea04bffa7f6a0da571fc70e
markolozajic <marko.lozajic@student.uni-tuebingen.de>
2017-07-10 15:42:23 +0200

    POSTagging methods now return arrays

--- src/POSTagging.java
+++ src/POSTagging.java
@@ -1,24 +1,71 @@
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 import java.io.*;
 
+
 public class POSTagging {
+
+    private static String readSentencesFromFile(String filename) throws IOException{
+        String[] anArray = sentenceDetector(filename);
+
+        String sentences = "";
+
+        for (String str : anArray) {
+            sentences += str + "\n";
+        }
+
+        return sentences;
+    }
+
+    private static String[] sentenceDetector (String filename) throws IOException {
+
+        BufferedReader buff = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+        String nextLine = "";
+        String bop = "";
+        while ((nextLine = buff.readLine()) != null) {
+            bop += nextLine + " ";
+        }
+
+        InputStream modelIn = new FileInputStream("opennlp-master/models/en-sent.bin");
+        SentenceModel model = new SentenceModel(modelIn);
+        SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+        modelIn.close();
+
+        return sentenceDetector.sentDetect(bop);
+    }
+
+    private static String[] tokenizer(String sentences) throws IOException {
+
+        InputStream modelIn = new FileInputStream("opennlp-master/models/en-token.bin");
+        TokenizerModel model = new TokenizerModel(modelIn);
+        Tokenizer tokenizer = new TokenizerME(model);
+        modelIn.close();
+
+        return tokenizer.tokenize(sentences);
+    }
+
+    private static String[] postagger(String[] tokenizedText) throws IOException {
+
+        InputStream modelIn = new FileInputStream("opennlp-master/models/en-pos-maxent.bin");
+        POSModel model = new POSModel(modelIn);
+        POSTaggerME tagger = new POSTaggerME(model);
+        modelIn.close();
+
+        return tagger.tag(tokenizedText);
+    }
+
     public static void main (String[] args) throws IOException{
-
-            File testfile = new File("testfile.txt");
-
-            ProcessBuilder sentences = new ProcessBuilder("opennlp-master/bin/opennlp", "SentenceDetector", "opennlp-master/models/en-sent.bin");
-            sentences.redirectInput(testfile);
-            sentences.redirectOutput(new File("sentences.txt"));
-            sentences.start();
-
-            ProcessBuilder tokenizer = new ProcessBuilder("opennlp-master/bin/opennlp","TokenizerME","opennlp-master/models/en-token.bin");
-            tokenizer.redirectInput(new File("sentences.txt"));
-            tokenizer.redirectOutput(new File("tokenized.txt"));
-            tokenizer.start();
-
-            ProcessBuilder posTagger = new ProcessBuilder("opennlp-master/bin/opennlp","POSTagger","opennlp-master/models/en-pos-maxent.bin");
-            posTagger.redirectInput(new File("tokenized.txt"));
-            posTagger.redirectOutput(new File("tagged.txt"));
-            posTagger.start();
+        String contents = readSentencesFromFile("testfile.txt");
+        String[] tokenized = tokenizer(contents);
+        String[] tags = postagger(tokenized);
+        for(int i = 0; i<tokenized.length; i++){
+            System.out.println(tokenized[i] + " " + tags[i]);
+        }
     }
 }
